63acf610a798040f450be6f3df66e82ece5f87bf
ShantoOBS <roys22127@gmail.com>
2025-11-13 01:40:13 +0600

    Set up server and connect with mongoDB

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ async function run() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-
+    
     
 
 
